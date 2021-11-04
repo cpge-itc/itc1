2ccec9d0f1cda3fe9ac8d02b424f4dea7b7f1c97
MTlmd <93731588+MTlmd@users.noreply.github.com>
2021-11-04 20:53:12 +0100

    Update dm1.py
    Bonsoir Monsieur,
    Voici mon DM. Par ailleurs, pour la question 2, puisque l'on veut que la fonction renvoie jusqu'à 2**n, il faut que par exemple puissance2(4) renvoie aussi 16 et ne s'arrête pas à 8. C'est pourquoi j'ai choisi d'écrire "range(n+1)" à la place de "range(n)".
    Cordialement,
    Martin Toulemonde MPSI

--- dm/dm1.py
+++ dm/dm1.py
@@ -3,16 +3,52 @@
 ## 1. Écrire une fonction somme telle que somme(n) renvoie la somme des entiers de 1 à n.
 ## Par exemple, somme(4) renvoie 10 (= 1 + 2 + 3 + 4).
 
+def somme(n):
+    s=0
+    for i in range (n+1):
+        s=s+i
+    return s
+
+
 ## 2. Écrire une fonction puissance2 telle que puissance2(n) renvoie la liste
 ## contenant les puissances de 2 jusqu'à 2**n.
 ## Par exemple, puissance2(4) renvoie [1, 2, 4, 8] (car 2**0 = 1, 2**1 = 2, 2**2 = 4, 2**3 = 8).
+
+def puissance2(n):
+    L=[]
+    for i in range (n+1):
+        L.append(2**i)
+    return L
+
 
 ## 3. Écrire une fonction produit telle que, si L est une liste de nombre,
 ## produit(L) renvoie la multiplication de tous les éléments de L
 ## Par exemple, produit([2, 1, 5, 4]) doit renvoyer 40 (= 2*1*5*4)
 
+def produit(L):
+    p=1
+    for i in range (len(L)):
+        p=p*L[i]
+    return p
+
+
 ## 4. Écrire une fonction egal telle que egal(L) renvoie True si tous les éléments de L sont égaux, False sinon.
 ## Par exemple, egal([1, 1, 1, 1]) doit renvoyer True, egal([1, 2]) doit renvoyer False.
 
+def egal(L):
+    s=L[0]
+    for i in range (len(L)):
+        if s!=L[i]:
+            return False
+    return True
+
+
 ## 5. Écrire une fonction carre_parfait telle que carre_parfait(n) renvoie True si n peut être écrit comme le carré d'un entier, False sinon.
 ## Par exemple, carre_parfait(9) doit renvoyer True (car 9 = 3²), carre_parfait(5) doit renvoyer False.
+
+def carre_parfait(n):
+    for i in range (n):
+        if i==n**0.5:
+            return True
+    return False
+
